0848a85eddc5549ea2c620f11584248840376300
Amilcar Malaver <ajmr182@gmail.com>
2025-05-06 18:38:01 -0500

    feat: Add login logic

--- src/main/kotlin/com/ajdev/artic/di/DatabaseModule.kt
+++ src/main/kotlin/com/ajdev/artic/di/DatabaseModule.kt
@@ -1,5 +1,6 @@
 package com.ajdev.artic.di
 
+import com.ajdev.artic.data.SupabaseSetup
 import com.ajdev.artic.data.local.dao.ProductsDao
 import com.ajdev.artic.data.local.dao.ProductsDaoImpl
 import com.ajdev.artic.data.repository.ProductRepository
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val databaseModule = module {
     single<ProductsDao> { ProductsDaoImpl() }
     single { ProductRepository(get()) }
+    single { SupabaseSetup() }
 }
--- src/main/kotlin/com/ajdev/artic/ui/login/LoginViewModel.kt
+++ src/main/kotlin/com/ajdev/artic/ui/login/LoginViewModel.kt
@@ -1,26 +1,29 @@
 package com.ajdev.artic.ui.login
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ajdev.artic.domain.usecase.LoginUseCase
 import com.ajdev.artic.ui.base.BaseViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import kotlin.coroutines.cancellation.CancellationException
 
-class LoginViewModel : BaseViewModel() {
+class LoginViewModel(private val loginUseCase: LoginUseCase) : BaseViewModel() {
     private val _email = MutableStateFlow("")
     val email: StateFlow<String> = _email
 
     private val _password = MutableStateFlow("")
     val password: StateFlow<String> = _password
 
+    private val _loginEvent = MutableSharedFlow<Boolean>()
+    val loginEvent = _loginEvent.asSharedFlow()
+
     fun login() {
         viewModelScope.launch(Dispatchers.Default) {
-
+            val success = loginUseCase.signIn(email.value, password.value)
+            _loginEvent.emit(success)
         }
     }
 
